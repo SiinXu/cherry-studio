ab4b77b948d534548cbd473a6d4d599c7fc5788d
Siin <31815270+SiinXu@users.noreply.github.com>
2025-03-07 00:06:47 +0800

    修复合并上游代码后的依赖和版本问题

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -33,7 +33,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'cherry-studio',
   storage,
-  version: 77,
+  version: 78,
   blacklist: ['runtime'],
   migrate,
   debug: true,
