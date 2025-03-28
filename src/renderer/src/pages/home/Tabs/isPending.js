c91674ac73a6c0e1c222b1fa5483ea0b19f81d23
Siin Xu <31815270+SiinXu@users.noreply.github.com>
2025-03-28 12:20:41 +0800

    update

---
+++ src/renderer/src/pages/home/Tabs/isPending.js
@@ -0,0 +1,1 @@
+const isPending = (topicId) => hasTopicPendingRequests(topicId);
---
+++ src/renderer/src/utils/pending.ts
@@ -0,0 +1,1 @@
+// 定义函数用于判断话题是否处于等待状态 export const isPending = (topicId: string) => hasTopicPendingRequests(topicId);
